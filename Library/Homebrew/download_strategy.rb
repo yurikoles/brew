efa2786732cdf4a0662029fba5ec6eaba521ffc5
Justin Rackliffe <justin.rackliffe@fmr.com>
2025-05-02 15:00:22 -0400

    Resolving CI style errors with whitespace that are not flagged using stle locally

--- Library/Homebrew/download_strategy.rb
+++ Library/Homebrew/download_strategy.rb
@@ -707,10 +707,10 @@ class CurlGitHubPackagesDownloadStrategy < CurlDownloadStrategy
     # HOMEBREW_GITHUB_PACKAGES_AUTH set in brew.sh
     # If using a private GHCR mirror with no Authentication set than do not add the header. In all other cases add it.
     if !Homebrew::EnvConfig.artifact_domain.presence ||
-      Homebrew::EnvConfig.docker_registry_basic_auth_token.presence ||
-      Homebrew::EnvConfig.docker_registry_token.presence
-     meta[:headers] << "Authorization: #{HOMEBREW_GITHUB_PACKAGES_AUTH}"
-   end
+       Homebrew::EnvConfig.docker_registry_basic_auth_token.presence ||
+       Homebrew::EnvConfig.docker_registry_token.presence
+      meta[:headers] << "Authorization: #{HOMEBREW_GITHUB_PACKAGES_AUTH}"
+    end
     super
   end
 
