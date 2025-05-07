4373aad1532a495402a7a3afb70c356b7aa399ba
botantony <antonsm21@gmail.com>
2025-05-07 14:07:36 +0200

    cask/dsl: set `no_autobump!` if `:extract_plist` livecheck strategy is used
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Library/Homebrew/autobump_constants.rb
+++ Library/Homebrew/autobump_constants.rb
@@ -5,4 +5,5 @@
 NO_AUTOBUMP_REASONS_LIST = T.let({
   incompatible_version_format: "incompatible version format",
   bumped_by_upstream:          "bumped by upstream",
+  extract_plist:               "livecheck uses `:extract_plist` strategy",
 }.freeze, T::Hash[Symbol, String])
