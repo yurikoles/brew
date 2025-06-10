ea8152757e4ddde54ea83eb03d6071c689b70078
Eric Knibbe <enk3@outlook.com>
2025-06-10 13:51:06 -0400

    rubocops/cask/no_overrides: avoid nested `depends_on macos:`

--- Library/Homebrew/test/support/fixtures/cask/Casks/with-depends-on-macos-failure.rb
+++ Library/Homebrew/test/support/fixtures/cask/Casks/with-depends-on-macos-failure.rb
@@ -1,20 +1,18 @@
 cask "with-depends-on-macos-failure" do
-  version "1.2.3"
-  sha256 "67cdb8a02803ef37fdbf7e0be205863172e41a561ca446cd84f0d7ab35a99d94"
-
   # guarantee a mismatched release
-  on_mojave :or_older do
-    depends_on macos: :catalina
+  on_big_sur :or_older do
+    version "1.2.3"
+    sha256 "67cdb8a02803ef37fdbf7e0be205863172e41a561ca446cd84f0d7ab35a99d94"
   end
-  on_catalina do
-    depends_on macos: :mojave
-  end
-  on_big_sur :or_newer do
-    depends_on macos: :catalina
+  on_ventura :or_newer do
+    version "1.2.3"
+    sha256 "67cdb8a02803ef37fdbf7e0be205863172e41a561ca446cd84f0d7ab35a99d94"
   end
 
   url "file://#{TEST_FIXTURE_DIR}/cask/caffeine.zip"
   homepage "https://brew.sh/with-depends-on-macos-failure"
+
+  depends_on macos: :monterey
 
   app "Caffeine.app"
 end
