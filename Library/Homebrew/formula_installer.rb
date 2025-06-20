052c0ae2cd18518e83281383b5fa7df94ebccaf4
thibhero <thibaut.herault@gmail.com>
2025-06-20 12:25:12 -0400

    adding parameter to fetch quietly if wanted

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -1355,12 +1355,12 @@ on_request: installed_on_request?, options:)
     end
   end
 
-  sig { void }
-  def fetch_bottle_tab
+  sig { params(quiet: T::Boolean).void }
+  def fetch_bottle_tab(quiet: false)
     return if @fetch_bottle_tab
 
     begin
-      formula.fetch_bottle_tab
+      formula.fetch_bottle_tab(quiet: quiet)
       @bottle_tab_runtime_dependencies = formula.bottle_tab_attributes
                                                 .fetch("runtime_dependencies", []).then { |deps| deps || [] }
                                                 .each_with_object({}) { |dep, h| h[dep["full_name"]] = dep }
