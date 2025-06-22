431d8f1ff748228aab9407811bf70594f8c565e5
Rylan Polster <rslpolster@gmail.com>
2025-06-21 21:40:57 -0400

    Consistently use `ClassMethods` sub-module for prepending class methods

--- Library/Homebrew/extend/os/linux/cask/quarantine.rb
+++ Library/Homebrew/extend/os/linux/cask/quarantine.rb
@@ -5,18 +5,20 @@ module OS
   module Linux
     module Cask
       module Quarantine
-        extend T::Helpers
+        module ClassMethods
+          extend T::Helpers
 
-        requires_ancestor { ::Cask::Quarantine }
+          requires_ancestor { ::Cask::Quarantine }
 
-        sig { returns(Symbol) }
-        def check_quarantine_support = :linux
+          sig { returns(Symbol) }
+          def check_quarantine_support = :linux
 
-        sig { returns(T::Boolean) }
-        def available? = false
+          sig { returns(T::Boolean) }
+          def available? = false
+        end
       end
     end
   end
 end
 
-Cask::Quarantine.singleton_class.prepend(OS::Linux::Cask::Quarantine)
+Cask::Quarantine.singleton_class.prepend(OS::Linux::Cask::Quarantine::ClassMethods)
--- Library/Homebrew/extend/os/linux/simulate_system.rb
+++ Library/Homebrew/extend/os/linux/simulate_system.rb
@@ -4,25 +4,27 @@
 module OS
   module Linux
     module SimulateSystem
-      sig { returns(T.nilable(Symbol)) }
-      def os
-        @os ||= T.let(nil, T.nilable(Symbol))
-        return :macos if @os.blank? && Homebrew::EnvConfig.simulate_macos_on_linux?
+      module ClassMethods
+        sig { returns(T.nilable(Symbol)) }
+        def os
+          @os ||= T.let(nil, T.nilable(Symbol))
+          return :macos if @os.blank? && Homebrew::EnvConfig.simulate_macos_on_linux?
 
-        @os
-      end
+          @os
+        end
 
-      sig { returns(T::Boolean) }
-      def simulating_or_running_on_linux?
-        os.blank? || os == :linux
-      end
+        sig { returns(T::Boolean) }
+        def simulating_or_running_on_linux?
+          os.blank? || os == :linux
+        end
 
-      sig { returns(Symbol) }
-      def current_os
-        os || :linux
+        sig { returns(Symbol) }
+        def current_os
+          os || :linux
+        end
       end
     end
   end
 end
 
-Homebrew::SimulateSystem.singleton_class.prepend(OS::Linux::SimulateSystem)
+Homebrew::SimulateSystem.singleton_class.prepend(OS::Linux::SimulateSystem::ClassMethods)
