0fb87af72c5d8fc44c7902baffdbae24f24e393e
botantony <antonsm21@gmail.com>
2025-06-24 16:38:11 +0200

    autobump_constants: split `NO_AUTOBUMP_REASONS_LIST`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Library/Homebrew/autobump_constants.rb
+++ Library/Homebrew/autobump_constants.rb
@@ -1,11 +1,15 @@
 # typed: strict
 # frozen_string_literal: true
 
-# TODO: add more reasons here
+NO_AUTOBUMP_REASONS_INTERNAL = T.let({
+  extract_plist:  "livecheck uses `:extract_plist` strategy",
+  latest_version: "`version` is set to `:latest`",
+}.freeze, T::Hash[Symbol, String])
+
+# The valid symbols for passing to `no_autobump!` in a `Formula` or `Cask`.
+# @api public
 NO_AUTOBUMP_REASONS_LIST = T.let({
   incompatible_version_format: "incompatible version format",
   bumped_by_upstream:          "bumped by upstream",
-  extract_plist:               "livecheck uses `:extract_plist` strategy",
-  latest_version:              "`version` is set to `:latest`",
   requires_manual_review:      "a manual review of this package is required for inclusion in autobump",
-}.freeze, T::Hash[Symbol, String])
+}.merge(NO_AUTOBUMP_REASONS_INTERNAL).freeze, T::Hash[Symbol, String])
