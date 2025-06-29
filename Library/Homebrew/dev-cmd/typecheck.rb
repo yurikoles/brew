836d85277fa085d16a3054e128d91ce6da51b540
Issy Long <me@issylong.com>
2025-06-29 18:49:57 +0100

    Keep only the parts of `rubocop.rbi` that we actually use
    - This file was _massive_ - over 60k lines and we had to bump the file
      size limit for pushes to the repo!
    - This was because by default Tapioca, when it encounters a
      `require "rubocop"` during RBI generation, loads all of the cops ever
      because they're all classes inside `RuboCop::Cop`.
    - There wasn't an easy way to control this at Tapioca generation time
      (we tried), so now we parse the generated RBI file and delete classes
      and method definitions that we don't use.
    - I regenerated the RBIs (`brew tc --update rubocop`) and added new
      things to the allowlist until Sorbet came back green.
    - Now the file is ~7k lines and 240K - much better!

--- Library/Homebrew/dev-cmd/typecheck.rb
+++ Library/Homebrew/dev-cmd/typecheck.rb
@@ -68,6 +68,9 @@ module Homebrew
             ohai "Updating Tapioca RBI files..."
             safe_system "bundle", "exec", "tapioca", "gem", *tapioca_args
 
+            ohai "Trimming RuboCop RBI because by default it's massive..."
+            trim_rubocop_rbi
+
             if args.suggest_typed?
               ohai "Checking if we can bump Sorbet `typed` sigils..."
               # --sorbet needed because of https://github.com/Shopify/spoom/issues/488
@@ -115,6 +118,157 @@ module Homebrew
                        "more information on how to resolve these errors."
           Homebrew.failed = true
         end
+      end
+
+      sig { params(path: T.any(String, Pathname)).void }
+      def trim_rubocop_rbi(path: HOMEBREW_LIBRARY_PATH/"sorbet/rbi/gems/rubocop@*.rbi")
+        rbi_file = Dir.glob(path).first
+        return unless rbi_file.present?
+        return unless (rbi_path = Pathname.new(rbi_file)).exist?
+
+        require "prism"
+        original_content = rbi_path.read
+        parsed = Prism.parse(original_content)
+        return unless parsed.success?
+
+        allowlist = %w[
+          Parser::Source
+          RuboCop::AST::Node
+          RuboCop::AST::NodePattern
+          RuboCop::AST::ProcessedSource
+          RuboCop::CLI
+          RuboCop::Config
+          RuboCop::Cop::AllowedPattern
+          RuboCop::Cop::AllowedMethods
+          RuboCop::Cop::AutoCorrector
+          RuboCop::Cop::AutocorrectLogic
+          RuboCop::Cop::Base
+          RuboCop::Cop::CommentsHelp
+          RuboCop::Cop::ConfigurableFormatting
+          RuboCop::Cop::ConfigurableNaming
+          RuboCop::Cop::Corrector
+          RuboCop::Cop::IgnoredMethods
+          RuboCop::Cop::IgnoredNode
+          RuboCop::Cop::IgnoredPattern
+          RuboCop::Cop::MethodPreference
+          RuboCop::Cop::Offense
+          RuboCop::Cop::RangeHelp
+          RuboCop::Cop::Registry
+          RuboCop::Cop::Util
+          RuboCop::DirectiveComment
+          RuboCop::Error
+          RuboCop::ExcludeLimit
+          RuboCop::Ext::Comment
+          RuboCop::Ext::ProcessedSource
+          RuboCop::Ext::Range
+          RuboCop::FileFinder
+          RuboCop::Formatter::TextUtil
+          RuboCop::Formatter::PathUtil
+          RuboCop::Options
+          RuboCop::ResultCache
+          RuboCop::Runner
+          RuboCop::TargetFinder
+          RuboCop::Version
+        ].freeze
+
+        nodes_to_keep = Set.new
+
+        parsed.value.statements.body.each do |node|
+          case node
+          when Prism::ModuleNode, Prism::ClassNode
+            # Keep if it's in our allowlist or is a top-level essential node.
+            full_name = extract_full_name(node)
+            nodes_to_keep << node if full_name.blank? || allowlist.any? { |name| full_name.start_with?(name) }
+          when Prism::ConstantWriteNode # Keep essential constants.
+            nodes_to_keep << node if node.name.to_s.match?(/^[[:digit:][:upper:]_]+$/)
+          else # Keep other top-level nodes (comments, etc.)
+            nodes_to_keep << node
+          end
+        end
+
+        new_content = generate_trimmed_rbi(original_content, nodes_to_keep, parsed)
+        rbi_path.write(new_content)
+      end
+
+      private
+
+      sig { params(node: Prism::Node).returns(String) }
+      def extract_full_name(node)
+        case node
+        when Prism::ModuleNode, Prism::ClassNode
+          parts = []
+
+          constant_path = node.constant_path
+          if constant_path.is_a?(Prism::ConstantReadNode)
+            parts << constant_path.name.to_s
+          elsif constant_path.is_a?(Prism::ConstantPathNode)
+            parts.concat(extract_constant_path_parts(constant_path))
+          end
+
+          parts.join("::")
+        else
+          ""
+        end
+      end
+
+      sig { params(constant_path: T.any(Prism::ConstantPathNode, Prism::Node)).returns(T::Array[String]) }
+      def extract_constant_path_parts(constant_path)
+        parts = []
+        current = T.let(constant_path, T.nilable(Prism::Node))
+
+        while current
+          case current
+          when Prism::ConstantPathNode
+            parts.unshift(current.name.to_s)
+            current = current.parent
+          when Prism::ConstantReadNode
+            parts.unshift(current.name.to_s)
+            break
+          else
+            break
+          end
+        end
+
+        parts
+      end
+
+      sig {
+        params(
+          original_content: String,
+          nodes_to_keep:    T::Set[Prism::Node],
+          parsed:           Prism::ParseResult,
+        ).returns(String)
+      }
+      def generate_trimmed_rbi(original_content, nodes_to_keep, parsed)
+        lines = original_content.lines
+        output_lines = []
+
+        first_node = parsed.value.statements.body.first
+        if first_node
+          first_line = first_node.location.start_line - 1
+          (0...first_line).each { |i| output_lines << lines[i] if lines[i] }
+        end
+
+        parsed.value.statements.body.each do |node|
+          next unless nodes_to_keep.include?(node)
+
+          start_line = node.location.start_line - 1
+          end_line = node.location.end_line - 1
+
+          (start_line..end_line).each { |i| output_lines << lines[i] if lines[i] }
+          output_lines << "\n"
+        end
+
+        header = <<~EOS.chomp
+          # typed: true
+
+          # This file is autogenerated. Do not edit it by hand.
+          # To regenerate, run `brew typecheck --update rubocop`.
+        EOS
+
+        return header if output_lines.empty?
+
+        output_lines.join
       end
     end
   end
