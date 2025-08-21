12366f9c5ba57c85d2a12f781e3e9fe3324e075b
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-08-21 22:33:29 +0800

    bundle/commands/install: `typed: strict`

--- Library/Homebrew/bundle/commands/install.rb
+++ Library/Homebrew/bundle/commands/install.rb
@@ -1,4 +1,4 @@
-# typed: true # rubocop:todo Sorbet/StrictSigil
+# typed: strict
 # frozen_string_literal: true
 
 require "bundle/brewfile"
@@ -8,6 +8,17 @@ module Homebrew
   module Bundle
     module Commands
       module Install
+        sig {
+          params(
+            global:     T::Boolean,
+            file:       T.nilable(String),
+            no_lock:    T::Boolean,
+            no_upgrade: T::Boolean,
+            verbose:    T::Boolean,
+            force:      T::Boolean,
+            quiet:      T::Boolean,
+          ).void
+        }
         def self.run(global: false, file: nil, no_lock: false, no_upgrade: false, verbose: false, force: false,
                      quiet: false)
           @dsl = Brewfile.read(global:, file:)
@@ -17,7 +28,9 @@ module Homebrew
           ) || exit(1)
         end
 
+        sig { returns(T.nilable(Dsl)) }
         def self.dsl
+          @dsl ||= T.let(nil, T.nilable(Dsl))
           @dsl
         end
       end
