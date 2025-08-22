4a681717cea3805042e8c41c65c9915a101e39b5
Carlo Cabrera <github@carlo.cab>
2025-08-22 17:52:18 +0800

    formula_cellar_checks: check unversioned name in `universal_binary_allowlist`
    This will allow us to avoid having to list multiple formula versions in
    the universal binary allowlist (e.g. for `llvm`).

--- Library/Homebrew/formula_cellar_checks.rb
+++ Library/Homebrew/formula_cellar_checks.rb
@@ -371,7 +371,10 @@ module FormulaCellarChecks
     mismatches = mismatches.to_h
 
     universal_binaries_expected = if (formula_tap = formula.tap).present? && formula_tap.core_tap?
-      formula_tap.audit_exception(:universal_binary_allowlist, formula.name)
+      formula_name = formula.name
+      # Apply audit exception to versioned formulae too from the unversioned name.
+      formula_name.gsub!(/@\d+(\.\d+)*$/, "") if formula.versioned_formula?
+      formula_tap.audit_exception(:universal_binary_allowlist, formula_name)
     else
       true
     end
