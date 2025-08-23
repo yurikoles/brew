73bb781d1d6bd6959621acc89a831ecbd27113b6
Douglas Eichelberger <d@eic.email>
2025-08-23 09:23:25 -0700

    Enable strict typing in yard extensions

--- Library/Homebrew/yard/templates/default/docstring/html/setup.rb
+++ Library/Homebrew/yard/templates/default/docstring/html/setup.rb
@@ -1,12 +1,13 @@
-# typed: true # rubocop:todo Sorbet/StrictSigil
+# typed: strict
 # frozen_string_literal: true
 
 # This follows the docs at https://github.com/lsegal/yard/blob/main/docs/Templates.md#setuprb
 # rubocop:disable Style/TopLevelMethodDefinition
+sig { void }
 def init
   # `sorbet` is available transitively through the `yard-sorbet` plugin, but we're
   # outside of the standalone sorbet config, so `checked` is enabled by default
-  T.bind(self, T.all(Class, YARD::Templates::Template), checked: false)
+  T.bind(self, T.all(T::Class[T.anything], YARD::Templates::Template), checked: false)
   super
 
   return if sections.empty?
@@ -14,6 +15,7 @@ def init
   sections[:index].place(:internal).before(:private)
 end
 
+sig { returns(T.nilable(String)) }
 def internal
   T.bind(self, YARD::Templates::Template, checked: false)
   erb(:internal) if object.has_tag?(:api) && object.tag(:api).text == "internal"
