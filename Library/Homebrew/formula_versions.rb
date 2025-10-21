yurikoles/brew

commit fa68f3d30f3176aa5cce9928d1d43bfa37085edc

Add `Formula#tap_path` method for theoretical formula file location in tap

Library/Homebrew/formula_versions.rb
@@ -20,7 +20,7 @@ class FormulaVersions
   sig { params(formula: Formula).void }
   def initialize(formula)
     @name = formula.name
-    @path = formula.path
+    @path = formula.tap_path
     @repository = T.must(formula.tap).path
     @relative_path = @path.relative_path_from(repository).to_s
     # Also look at e.g. older homebrew-core paths before sharding.

Library/Homebrew/dev-cmd/bottle.rb
@@ -661,8 +661,8 @@ module Homebrew
             "formula" => {
               "name"             => formula.name,
               "pkg_version"      => formula.pkg_version.to_s,
-              "path"             => formula.path.to_s.delete_prefix("#{HOMEBREW_REPOSITORY}/"),
-              "tap_git_path"     => formula.path.to_s.delete_prefix("#{tap_path}/"),
+              "path"             => formula.tap_path.to_s.delete_prefix("#{HOMEBREW_REPOSITORY}/"),
+              "tap_git_path"     => formula.tap_path.to_s.delete_prefix("#{tap_path}/"),
               "tap_git_revision" => tap_git_revision,
               "tap_git_remote"   => tap_git_remote,
               # descriptions can contain emoji. sigh.

Library/Homebrew/formula.rb
@@ -426,6 +426,13 @@ class Formula
   sig { returns(T.nilable(String)) }
   def full_installed_alias_name = full_name_with_optional_tap(installed_alias_name)
 
+  sig { returns(Pathname) }
+  def tap_path
+    return path unless (t = tap)
+
+    t.new_formula_path(name)
+  end
+
   # The path that was specified to find this formula.
   sig { returns(T.nilable(Pathname)) }
   def specified_path

commit b00f82f4675a6cf3f5213e98b1d0739b4f06f02c

formula_versions: fix tracking previous versions of formulae
This broke in fa68f3d30f (#20895). Since `Formula#tap_path` returns the
theoretical/canonical location within a tap that a formula will live, it
may not correspond to the actual location of the formula file (e.g.,
when a non-official tap is sharded). Using `Formula#path` instead
correctly tracks the actual file location.

Fixes #20917.

Library/Homebrew/formula_versions.rb
@@ -20,7 +20,7 @@ class FormulaVersions
   sig { params(formula: Formula).void }
   def initialize(formula)
     @name = formula.name
-    @path = formula.tap_path
+    @path = formula.path
     @repository = T.must(formula.tap).path
     @relative_path = @path.relative_path_from(repository).to_s
     # Also look at e.g. older homebrew-core paths before sharding.