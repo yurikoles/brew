d6922e847ae7d3d7d568de6b555bfa3c2767fea3
XXXXRT666 <157766680+XXXXRT666@users.noreply.github.com>
2025-11-18 07:40:16 +0000

    Use Correct Links For Brew Mirrors

--- Library/Homebrew/cmd/vendor-install.sh
+++ Library/Homebrew/cmd/vendor-install.sh
@@ -56,7 +56,7 @@ set_ruby_variables() {
     fi
     if [[ -n "${HOMEBREW_BOTTLE_DOMAIN}" ]]
     then
-      ruby_URLs+=("${HOMEBREW_BOTTLE_DOMAIN}/bottles-portable-ruby/${ruby_FILENAME}")
+      ruby_URLs+=("${HOMEBREW_BOTTLE_DOMAIN}/${ruby_FILENAME}")
     fi
     ruby_URLs+=(
       "https://ghcr.io/v2/homebrew/core/portable-ruby/blobs/sha256:${ruby_SHA}"
