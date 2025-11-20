1c48996a5bcd865cd52f2c2a7b2074f9c8a7f520
Michael Cho <michael@michaelcho.dev>
2025-11-20 13:19:19 -0500

    utils/linkage: allow checking non-brew libraries

--- Library/Homebrew/utils/linkage.rb
+++ Library/Homebrew/utils/linkage.rb
@@ -6,10 +6,12 @@ module Utils
     params(binary: T.any(String, Pathname), library: T.any(String, Pathname)).returns(T::Boolean)
   }
   def self.binary_linked_to_library?(binary, library)
-    Pathname.new(binary).dynamically_linked_libraries.any? do |dll|
-      next false unless dll.start_with?(HOMEBREW_PREFIX.to_s)
+    library = library.to_s
+    library = File.realpath(library) if library.start_with?(HOMEBREW_PREFIX.to_s)
 
-      File.realpath(dll) == File.realpath(Pathname.new(library))
+    Pathname.new(binary).dynamically_linked_libraries.any? do |dll|
+      dll = File.realpath(dll) if dll.start_with?(HOMEBREW_PREFIX.to_s)
+      dll == library
     end
   end
 end
