dfdef32c9a8d6cbfb46261ec87ce0992b2e3fff4
Michael Cho <michael@michaelcho.dev>
2025-12-03 21:05:49 -0500

    test: replace some install_test_formula with setup_test_formula
    We can fake an install with just a tab file to avoid the overhead needed
    to run all of `install_test_formula`. This reduces time for modified
    test cases by 50% or more.

--- Library/Homebrew/test/cmd/reinstall_spec.rb
+++ Library/Homebrew/test/cmd/reinstall_spec.rb
@@ -8,11 +8,10 @@ RSpec.describe Homebrew::Cmd::Reinstall do
   it_behaves_like "parseable arguments"
 
   it "reinstalls a Formula", :aggregate_failures, :integration_test do
-    install_test_formula "testball"
+    setup_test_formula "testball", tab_attributes: { installed_on_request: true }
 
     testball_bin = HOMEBREW_CELLAR/"testball/0.1/bin"
-    expect(testball_bin).to exist
-    FileUtils.rm_r(testball_bin)
+    expect(testball_bin).not_to exist
 
     expect { brew "reinstall", "testball" }
       .to output(/Reinstalling testball/).to_stdout
