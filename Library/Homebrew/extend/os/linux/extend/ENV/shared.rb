0ded2ad35e8b9349c0d18b02ba0d6d9cab3851c4
Michael Cho <michael@michaelcho.dev>
2025-12-13 18:14:29 -0500

    extend/ENV/shared: `typed: strict`

--- Library/Homebrew/extend/os/linux/extend/ENV/shared.rb
+++ Library/Homebrew/extend/os/linux/extend/ENV/shared.rb
@@ -1,13 +1,18 @@
-# typed: true # rubocop:todo Sorbet/StrictSigil
+# typed: strict
 # frozen_string_literal: true
 
 module OS
   module Linux
     module SharedEnvExtension
+      extend T::Helpers
+
+      requires_ancestor { ::SharedEnvExtension }
+
+      sig { returns(Symbol) }
       def effective_arch
-        if @build_bottle && @bottle_arch
-          @bottle_arch.to_sym
-        elsif @build_bottle
+        if build_bottle && (bottle_arch = self.bottle_arch)
+          bottle_arch.to_sym
+        elsif build_bottle
           ::Hardware.oldest_cpu
         elsif ::Hardware::CPU.intel? || ::Hardware::CPU.arm?
           :native
