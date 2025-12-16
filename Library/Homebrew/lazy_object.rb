74276721c9f7bb9c398af5c5ac0a105cff0a080f
hyuraku <32809703+hyuraku@users.noreply.github.com>
2025-12-16 22:11:13 +0900

    lazy_object: typed: strict

--- Library/Homebrew/lazy_object.rb
+++ Library/Homebrew/lazy_object.rb
@@ -1,20 +1,25 @@
-# typed: true # rubocop:todo Sorbet/StrictSigil
+# typed: strict
 # frozen_string_literal: true
 
 require "delegate"
 
 # An object which lazily evaluates its inner block only once a method is called on it.
 class LazyObject < Delegator
+  sig { params(callable: T.nilable(Proc)).void }
   def initialize(&callable)
+    @__callable__ = T.let(nil, T.untyped)
+    @__getobj__ = T.let(nil, T.untyped)
     super(callable)
   end
 
-  def __getobj__(&)
-    return @__getobj__ if defined?(@__getobj__)
+  sig { returns(T.untyped) }
+  def __getobj__
+    return @__getobj__ if defined?(@__getobj__) && !@__getobj__.nil?
 
     @__getobj__ = @__callable__.call
   end
 
+  sig { params(callable: T.untyped).void }
   def __setobj__(callable)
     @__callable__ = callable
   end
@@ -22,6 +27,7 @@ class LazyObject < Delegator
   # Forward to the inner object to make lazy objects type-checkable.
   #
   # @!visibility private
+  sig { params(klass: T.any(T::Module[T.anything], T::Class[T.anything])).returns(T::Boolean) }
   def is_a?(klass)
     # see https://sorbet.org/docs/faq#how-can-i-fix-type-errors-that-arise-from-super
     T.bind(self, T.untyped)
@@ -29,6 +35,9 @@ class LazyObject < Delegator
     __getobj__.is_a?(klass) || super
   end
 
+  sig { returns(T::Class[T.anything]) }
   def class = __getobj__.class
+
+  sig { returns(String) }
   def to_s = __getobj__.to_s
 end
