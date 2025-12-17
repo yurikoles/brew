9f6c66d9e275f625ed8192d1e2539f40d7b264b6
Carlo Cabrera <github@carlo.cab>
2025-12-17 18:56:03 +0800

    cask/artifact/app: fix up permissions for globally installed apps
    Some casks (e.g. `sourcetree`) install `app` artifacts with permissions
    that make them accessible only to the user that installed them. This is
    problematic on multi-user systems when the app is installed into a
    global location like `/Applications`.
    
    Let's handle that by fixing up permissions appropriately when we install
    an app artifact into a global location.
    
    I've had to make this `typed: true` to match the code it inherits from
    the `Moved` class, which also used `typed: true` instead of
    `typed: strict`.

--- Library/Homebrew/cask/artifact/app.rb
+++ Library/Homebrew/cask/artifact/app.rb
@@ -1,4 +1,4 @@
-# typed: strict
+# typed: true # rubocop:todo Sorbet/StrictSigil
 # frozen_string_literal: true
 
 require "cask/artifact/moved"
@@ -7,6 +7,22 @@ module Cask
   module Artifact
     # Artifact corresponding to the `app` stanza.
     class App < Moved
+      def install_phase(command: nil, **options)
+        super
+
+        return if target.parent.to_s != "/Applications"
+
+        odebug "Fixing up '#{target}' permissions for installation to '#{target.parent}'"
+        # Ensure that globally installed applications can be accessed by all users.
+        target.find do |child|
+          permissions = (child.executable? || child.directory?) ? "a+rx" : "a+r"
+          if child.writable?
+            FileUtils.chmod(permissions, child)
+          else
+            command.run!("chmod", args: [permissions, child], sudo: true)
+          end
+        end
+      end
     end
   end
 end
