330574eace66c7e4564552487314604e34dafebc
Jan-Otto Kröpke <mail@jkroepke.de>
2025-12-18 20:57:10 +0100

    feat: Allow omit Authentication header, if `HOMEBREW_DOCKER_REGISTRY_BASIC_AUTH_TOKEN` is set to `-`
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- Library/Homebrew/download_strategy.rb
+++ Library/Homebrew/download_strategy.rb
@@ -719,10 +719,13 @@ class CurlGitHubPackagesDownloadStrategy < CurlDownloadStrategy
     meta[:headers] ||= []
     # GitHub Packages authorization header.
     # HOMEBREW_GITHUB_PACKAGES_AUTH set in brew.sh
-    # If using a private GHCR mirror with no Authentication set then do not add the header. In all other cases add it.
-    if !Homebrew::EnvConfig.artifact_domain.presence ||
+    # If using a private GHCR mirror with no Authentication set or HOMEBREW_GITHUB_PACKAGES_AUTH is empty
+    # then do not add the header. In all other cases add it.
+    if HOMEBREW_GITHUB_PACKAGES_AUTH.presence && (
+       !Homebrew::EnvConfig.artifact_domain.presence ||
        Homebrew::EnvConfig.docker_registry_basic_auth_token.presence ||
        Homebrew::EnvConfig.docker_registry_token.presence
+     )
       meta[:headers] << "Authorization: #{HOMEBREW_GITHUB_PACKAGES_AUTH}"
     end
     super
