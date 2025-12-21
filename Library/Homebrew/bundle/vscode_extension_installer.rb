37d047a85d773bb0ac7158b15da7dabc0ff36ad0
Michael Cho <michael@michaelcho.dev>
2025-12-21 16:28:30 -0500

    bundle/vscode_extension_installer: `typed: strict`

--- Library/Homebrew/bundle/vscode_extension_installer.rb
+++ Library/Homebrew/bundle/vscode_extension_installer.rb
@@ -1,13 +1,15 @@
-# typed: true # rubocop:todo Sorbet/StrictSigil
+# typed: strict
 # frozen_string_literal: true
 
 module Homebrew
   module Bundle
     module VscodeExtensionInstaller
+      sig { void }
       def self.reset!
         @installed_extensions = nil
       end
 
+      sig { params(name: String, no_upgrade: T::Boolean, verbose: T::Boolean).returns(T::Boolean) }
       def self.preinstall!(name, no_upgrade: false, verbose: false)
         if !Bundle.vscode_installed? && Bundle.cask_installed?
           puts "Installing visual-studio-code. It is not currently installed." if verbose
@@ -24,6 +26,15 @@ module Homebrew
         true
       end
 
+      sig {
+        params(
+          name:       String,
+          preinstall: T::Boolean,
+          no_upgrade: T::Boolean,
+          verbose:    T::Boolean,
+          force:      T::Boolean,
+        ).returns(T::Boolean)
+      }
       def self.install!(name, preinstall: true, no_upgrade: false, verbose: false, force: false)
         return true unless preinstall
         return true if extension_installed?(name)
@@ -39,13 +50,18 @@ module Homebrew
         true
       end
 
+      sig { params(name: String).returns(T::Boolean) }
       def self.extension_installed?(name)
         installed_extensions.include? name.downcase
       end
 
+      sig { returns(T::Array[String]) }
       def self.installed_extensions
         require "bundle/vscode_extension_dumper"
-        @installed_extensions ||= Homebrew::Bundle::VscodeExtensionDumper.extensions
+        @installed_extensions ||= T.let(
+          Homebrew::Bundle::VscodeExtensionDumper.extensions,
+          T.nilable(T::Array[String]),
+        )
       end
     end
   end
