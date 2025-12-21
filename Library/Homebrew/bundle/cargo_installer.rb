bbdc7eb0973ac29cfdfc61f9027ae114bd5f626c
Michael Cho <michael@michaelcho.dev>
2025-12-21 16:50:31 -0500

    bundle/cargo_installer: `typed: strict`

--- Library/Homebrew/bundle/cargo_installer.rb
+++ Library/Homebrew/bundle/cargo_installer.rb
@@ -1,13 +1,15 @@
-# typed: true # rubocop:todo Sorbet/StrictSigil
+# typed: strict
 # frozen_string_literal: true
 
 module Homebrew
   module Bundle
     module CargoInstaller
+      sig { void }
       def self.reset!
         @installed_packages = nil
       end
 
+      sig { params(name: String, verbose: T::Boolean, _options: T.anything).returns(T::Boolean) }
       def self.preinstall!(name, verbose: false, **_options)
         unless Bundle.cargo_installed?
           puts "Installing rust for cargo. It is not currently installed." if verbose
@@ -24,6 +26,15 @@ module Homebrew
         true
       end
 
+      sig {
+        params(
+          name:       String,
+          preinstall: T::Boolean,
+          verbose:    T::Boolean,
+          force:      T::Boolean,
+          _options:   T.anything,
+        ).returns(T::Boolean)
+      }
       def self.install!(name, preinstall: true, verbose: false, force: false, **_options)
         return true unless preinstall
 
@@ -40,13 +51,15 @@ module Homebrew
         true
       end
 
+      sig { params(package: String).returns(T::Boolean) }
       def self.package_installed?(package)
         installed_packages.include? package
       end
 
+      sig { returns(T::Array[String]) }
       def self.installed_packages
         require "bundle/cargo_dumper"
-        @installed_packages ||= Homebrew::Bundle::CargoDumper.packages
+        @installed_packages ||= T.let(Homebrew::Bundle::CargoDumper.packages, T.nilable(T::Array[String]))
       end
     end
   end
