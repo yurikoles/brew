d0ac999a323b1cd195329be3ab0748df876bb3bf
Michael Cho <michael@michaelcho.dev>
2025-12-21 17:25:22 -0500

    bundle/cask_dumper: `typed: strict`

--- Library/Homebrew/bundle/cask_dumper.rb
+++ Library/Homebrew/bundle/cask_dumper.rb
@@ -1,20 +1,22 @@
-# typed: true # rubocop:todo Sorbet/StrictSigil
+# typed: strict
 # frozen_string_literal: true
 
 module Homebrew
   module Bundle
     module CaskDumper
+      sig { void }
       def self.reset!
         @casks = nil
         @cask_names = nil
-        @cask_hash = nil
         @cask_oldnames = nil
       end
 
+      sig { returns(T::Array[String]) }
       def self.cask_names
-        @cask_names ||= casks.map(&:to_s)
+        @cask_names ||= T.let(casks.map(&:to_s), T.nilable(T::Array[String]))
       end
 
+      sig { returns(T::Array[String]) }
       def self.outdated_cask_names
         return [] unless Bundle.cask_installed?
 
@@ -22,6 +24,7 @@ module Homebrew
              .map(&:to_s)
       end
 
+      sig { params(cask_name: String).returns(T::Boolean) }
       def self.cask_is_outdated_using_greedy?(cask_name)
         return false unless Bundle.cask_installed?
 
@@ -31,6 +34,7 @@ module Homebrew
         cask.outdated?(greedy: true)
       end
 
+      sig { params(describe: T::Boolean).returns(String) }
       def self.dump(describe: false)
         casks.map do |cask|
           description = "# #{cask.desc}\n" if describe && cask.desc.present?
@@ -39,25 +43,23 @@ module Homebrew
         end.join("\n")
       end
 
+      sig { returns(T::Hash[String, String]) }
       def self.cask_oldnames
-        return @cask_oldnames if @cask_oldnames
-
-        @cask_oldnames = {}
-        casks.each do |c|
+        @cask_oldnames ||= T.let(casks.each_with_object({}) do |c, hash|
           oldnames = c.old_tokens
           next if oldnames.blank?
 
           oldnames.each do |oldname|
-            @cask_oldnames[oldname] = c.full_name
+            hash[oldname] = c.full_name
             if c.full_name.include? "/" # tap cask
               tap_name = c.full_name.rpartition("/").first
-              @cask_oldnames["#{tap_name}/#{oldname}"] = c.full_name
+              hash["#{tap_name}/#{oldname}"] = c.full_name
             end
           end
-        end
-        @cask_oldnames
+        end, T.nilable(T::Hash[String, String]))
       end
 
+      sig { params(cask_list: T::Array[String]).returns(T::Array[String]) }
       def self.formula_dependencies(cask_list)
         return [] unless Bundle.cask_installed?
         return [] if cask_list.blank?
@@ -69,19 +71,21 @@ module Homebrew
         end.compact
       end
 
+      sig { returns(T::Array[Cask::Cask]) }
       private_class_method def self.casks
         return [] unless Bundle.cask_installed?
 
         require "cask/caskroom"
-        @casks ||= Cask::Caskroom.casks
+        @casks ||= T.let(Cask::Caskroom.casks, T.nilable(T::Array[Cask::Cask]))
       end
 
+      sig { params(cask_config: Cask::Config).returns(String) }
       private_class_method def self.explicit_s(cask_config)
         cask_config.explicit.map do |key, value|
           # inverse of #env - converts :languages config key back to --language flag
           if key == :languages
             key = "language"
-            value = cask_config.explicit.fetch(:languages, []).join(",")
+            value = Array(cask_config.explicit.fetch(:languages, [])).join(",")
           end
           "#{key}: \"#{value.to_s.sub(/^#{Dir.home}/, "~")}\""
         end.join(", ")
