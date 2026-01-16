dbca615d87aab5ef303202e5b35dc8a56a6d43b5
hyuraku <32809703+hyuraku@users.noreply.github.com>
2026-01-16 22:35:48 +0900

    refactor: change type annotations to strict in CacheStoreDatabase and CacheStore classes

--- Library/Homebrew/cache_store.rb
+++ Library/Homebrew/cache_store.rb
@@ -1,4 +1,4 @@
-# typed: true # rubocop:todo Sorbet/StrictSigil
+# typed: strict
 # frozen_string_literal: true
 
 require "json"
@@ -13,8 +13,9 @@ class CacheStoreDatabase
   #
   # @param  [Symbol] type
   # @yield  [CacheStoreDatabase] self
-  def self.use(type)
-    @db_type_reference_hash ||= {}
+  sig { params(type: T.untyped, _blk: T.proc.params(arg0: CacheStoreDatabase).returns(T.untyped)).returns(T.untyped) }
+  def self.use(type, &_blk)
+    @db_type_reference_hash ||= T.let({}, T.nilable(T::Hash[T.untyped, T.untyped]))
     @db_type_reference_hash[type] ||= {}
     type_ref = @db_type_reference_hash[type]
 
@@ -42,18 +43,21 @@ class CacheStoreDatabase
   #
   # @param  [Symbol] type
   # @return [nil]
+  sig { params(type: T.untyped).void }
   def initialize(type)
     @type = type
-    @dirty = false
+    @dirty = T.let(true, T.nilable(TrueClass))
   end
 
   # Sets a value in the underlying database (and creates it if necessary).
+  sig { params(key: T.untyped, value: T.untyped).void }
   def set(key, value)
     dirty!
     db[key] = value
   end
 
   # Gets a value from the underlying database (if it already exists).
+  sig { params(key: T.untyped).returns(T.untyped) }
   def get(key)
     return unless created?
 
@@ -61,6 +65,7 @@ class CacheStoreDatabase
   end
 
   # Deletes a value from the underlying database (if it already exists).
+  sig { params(key: T.untyped).void }
   def delete(key)
     return unless created?
 
@@ -69,6 +74,7 @@ class CacheStoreDatabase
   end
 
   # Deletes all content from the underlying database (if it already exists).
+  sig { void }
   def clear!
     return unless created?
 
@@ -77,6 +83,7 @@ class CacheStoreDatabase
   end
 
   # Closes the underlying database (if it is created and open).
+  sig { void }
   def write_if_dirty!
     return unless dirty?
 
@@ -87,6 +94,7 @@ class CacheStoreDatabase
   # Returns `true` if the cache file has been created for the given `@type`.
   #
   # @return [Boolean]
+  sig { returns(T::Boolean) }
   def created?
     cache_path.exist?
   end
@@ -94,6 +102,7 @@ class CacheStoreDatabase
   # Returns the modification time of the cache file (if it already exists).
   #
   # @return [Time]
+  sig { returns(T.nilable(Time)) }
   def mtime
     return unless created?
 
@@ -103,6 +112,7 @@ class CacheStoreDatabase
   # Performs a `select` on the underlying database.
   #
   # @return [Array]
+  sig { params(block: T.proc.params(arg0: T.untyped, arg1: T.untyped).returns(BasicObject)).returns(T::Hash[T.untyped, T.untyped]) }
   def select(&block)
     db.select(&block)
   end
@@ -110,6 +120,7 @@ class CacheStoreDatabase
   # Returns `true` if the cache is empty.
   #
   # @return [Boolean]
+  sig { returns(T::Boolean) }
   def empty?
     db.empty?
   end
@@ -117,6 +128,7 @@ class CacheStoreDatabase
   # Performs a `each_key` on the underlying database.
   #
   # @return [Array]
+  sig { params(block: T.proc.params(arg0: T.untyped).returns(BasicObject)).returns(T::Hash[T.untyped, T.untyped]) }
   def each_key(&block)
     db.each_key(&block)
   end
@@ -128,24 +140,31 @@ class CacheStoreDatabase
   # corresponding to the `@type` instance variable.
   #
   # @return [Hash] db
+  sig { returns(T::Hash[T.untyped, T.untyped]) }
   def db
-    @db ||= begin
-      JSON.parse(cache_path.read) if created?
+    @db ||= T.let({}, T.nilable(T::Hash[T.untyped, T.untyped]))
+    return @db if !@db.empty? || !created?
+
+    begin
+      result = JSON.parse(cache_path.read)
+      @db = result if result.is_a?(Hash)
     rescue JSON::ParserError
-      nil
+      # Ignore parse errors
     end
-    @db ||= {}
+    @db
   end
 
   # The path where the database resides in the `HOMEBREW_CACHE` for the given
   # `@type`.
   #
   # @return [String]
+  sig { returns(Pathname) }
   def cache_path
     HOMEBREW_CACHE/"#{@type}.json"
   end
 
   # Sets that the cache needs to be written to disk.
+  sig { void }
   def dirty!
     @dirty = true
   end
@@ -153,6 +172,7 @@ class CacheStoreDatabase
   # Returns `true` if the cache needs to be written to disk.
   #
   # @return [Boolean]
+  sig { returns(T.nilable(TrueClass)) }
   def dirty?
     @dirty
   end
@@ -165,14 +185,16 @@ end
 class CacheStore
   # @param  [CacheStoreDatabase] database
   # @return [nil]
+  sig { params(database: CacheStoreDatabase).void }
   def initialize(database)
-    @database = database
+    @database = T.let(database, CacheStoreDatabase)
   end
 
   # Inserts new values or updates existing cached values to persistent storage.
   #
   # @abstract
-  def update!(*)
+  sig { params(args: T.untyped).void }
+  def update!(*args)
     raise NotImplementedError
   end
 
@@ -180,19 +202,22 @@ class CacheStore
   # stored.
   #
   # @abstract
-  def fetch(*)
+  sig { params(args: T.untyped).returns(T.untyped) }
+  def fetch(*args)
     raise NotImplementedError
   end
 
   # Deletes data from the cache based on a condition defined in a concrete class.
   #
   # @abstract
-  def delete!(*)
+  sig { params(args: T.untyped).void }
+  def delete!(*args)
     raise NotImplementedError
   end
 
   protected
 
   # @return [CacheStoreDatabase]
+  sig { returns(CacheStoreDatabase) }
   attr_reader :database
 end
